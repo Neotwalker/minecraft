0279e5dcca6ee8fb27449c063f08d96273d5eedc
Neotwalker <and1marat9@gmail.com>
2023-01-17 19:27:43 +0500

    add

--- js/common.js
+++ js/common.js
@@ -69,6 +69,34 @@ $(function() {
 		]
 	});
 
+	$('.slider--versions').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		variableWidth: true,
+		arrows: true,
+		appendArrows:'.slider--arrows',
+		prevArrow: '<div class="arrows--prev version--prev"><svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.5 15L2.20711 8.70711C1.81658 8.31658 1.81658 7.68342 2.20711 7.29289L8.5 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>',
+		nextArrow: '<div class="arrows--next version--prev"><svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1L7.79289 7.29289C8.18342 7.68342 8.18342 8.31658 7.79289 8.70711L1.5 15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 10,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 6,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			},
+		]
+	});
+
 	$('.smiles .icon').click(function(){
 		if (!($(this).parent().find(".smiles--list").hasClass("open"))) {
 			$(this).parent().find(".smiles--list").fadeIn(300).addClass("open");
